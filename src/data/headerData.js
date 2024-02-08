7ffc41fb62c935333290b2dc4bcf798d07eeed58
Techbite <bonfacemwema7@gmail.com>
2024-02-08 10:21:03 +0300

    updated the projects data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,7 +1,7 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Boniface Muimi Mwema',
+    name: 'BONIFACE MUIMI MWEMA',
     title: "Web Developer and Mobile Application Developer",
     desciption:"Welcome to CodeCrafted, where I transform lines of code into digital works of art. As a passionate developer, I weave technology, creativity, and innovation into every project. Explore my portfolio to see how I breathe life into ideas, one line at a time. Whether it's web development, mobile apps, or software solutions, I turn concepts into reality. Let's connect and craft the future together.",
     image: 'https://cdn3.vectorstock.com/i/1000x1000/48/37/web-developer-design-vector-5884837.jpg',
