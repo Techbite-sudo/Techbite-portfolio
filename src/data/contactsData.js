9aad3ac25e440c1e47198f6d648d9e73f283ff2c
Techbite <bonfacemwema7@gmail.com>
2024-02-08 10:48:22 +0300

    added sheet API

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -2,6 +2,5 @@ export const contactsData = {
     email: 'bonfacemwema7@gmail.com',
     phone: '+254748271218',
     address: 'Nairobi Kenya - 00100 ',
-
-    sheetAPI: ''
+    sheetAPI: 'https://sheetdb.io/api/v1/e9r24nft55op1'
 }
